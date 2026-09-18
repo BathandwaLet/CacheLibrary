5c0bb9bea72811cdb4b1c89a7c0566de7217a268
Bathandwa <bmap750@gmail.com>
2026-09-18 13:05:24 +0200

    fix(Node): Changed Class fields to attributes to properties.
    feat(Node): Added constructor.

--- CacheLibrary.Core/Node.cs
+++ CacheLibrary.Core/Node.cs
@@ -2,8 +2,14 @@ namespace CacheLibrary.Core;
 
 public class Node <TKey, TValue>
 {
-    public TKey Key;
-    public TValue Value;
-    public Node<TKey, TValue> Prev;
-    public Node<TKey, TValue> Next;
+    public TKey Key { get; set; }
+    public TValue Value { get; set; }
+    public Node<TKey, TValue>? Prev { get; set; }
+    public Node<TKey, TValue>? Next { get; set; }
+    
+    public Node (TKey key, TValue value)
+    {
+        Key = key;
+        Value = value;
+    }
 }
