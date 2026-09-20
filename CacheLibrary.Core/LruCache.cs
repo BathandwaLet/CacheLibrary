c79771c505b11d45aaf0bcfbc18d567452005d43
Bathandwa <bmap750@gmail.com>
2026-09-20 19:51:08 +0200

    feat(LruCache): fully implemented TryGetValue method to fetch the process from the LruQueue and move to the front of the queue.

--- CacheLibrary.Core/LruCache.cs
+++ CacheLibrary.Core/LruCache.cs
@@ -15,7 +15,42 @@ public class LruCache<TKey, TValue>: ICache<TKey, TValue>
     //TODO get set remove to be defined
     public bool TryGetValue(TKey key, out TValue value)
     {
-        throw new NotImplementedException();
+        //Checking and then fetching the value
+        var hasKey = LruQueue.TryGetValue(key, out Node <TKey, TValue> node);
+        value = hasKey ? node.Value : default;
+        
+        //moving process to the front of the linkedlist
+        if (node == _head)
+        {
+            return hasKey;
+        }
+        else if (node.Prev != null)
+        {
+            //Unlink the node
+            // If the Node is tail
+            if (node.Next == null) 
+            {
+                node.Prev.Next = null;
+                _tail = node.Prev;
+            }
+            else
+            {
+                //if in between two nodes
+                node.Prev.Next = node.Next;
+                node.Next.Prev = node.Prev;
+            }
+            
+            //Link the new head
+            _head.Prev = node;
+            node.Next = _head;
+            node.Prev = null;
+            
+            //set the new head
+            _head = node;
+
+        }
+        
+        return hasKey ;
     }
 
     public bool TrySetValue(TKey key, TValue value)
