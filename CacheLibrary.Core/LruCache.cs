3195111e59514471b5cda968827a697836ce1fe2
Bathandwa <bmap750@gmail.com>
2026-09-21 10:44:50 +0200

    feat(TrySetValue): Full implemented a TrySetValue to set a value to a new node.
    refactor(MoveToHead): Created a new helper method to and moving nodes to the head of the linkedlist data strucuture.
    
    feat(InsertAtHead): Created a new helper method to handle inserting nodes in two cases.
     i- A completely empty linkedlist.
     ii- A non-empty linkedlist that then inserts that node at the head of the linkedlist.

--- CacheLibrary.Core/LruCache.cs
+++ CacheLibrary.Core/LruCache.cs
@@ -19,6 +19,7 @@ public class LruCache<TKey, TValue>: ICache<TKey, TValue>
         var hasKey = LruQueue.TryGetValue(key, out Node <TKey, TValue> node);
         value = hasKey ? node.Value : default;
         
+        
         //moving process to the front of the linkedlist
         if (!hasKey || node == _head)
         {
@@ -26,9 +27,11 @@ public class LruCache<TKey, TValue>: ICache<TKey, TValue>
         }
         else if (node.Prev != null)
         {
+            /*
+             Move to helper methods
             //Unlink the node
             // If the Node is tail
-            if (node.Next == null) 
+            if (node.Next == null)
             {
                 node.Prev.Next = null;
                 _tail = node.Prev;
@@ -39,15 +42,17 @@ public class LruCache<TKey, TValue>: ICache<TKey, TValue>
                 node.Prev.Next = node.Next;
                 node.Next.Prev = node.Prev;
             }
-            
+
+
             //Link the new head
             _head.Prev = node;
             node.Next = _head;
             node.Prev = null;
-            
+
             //set the new head
             _head = node;
-
+            */
+            MoveToHead(node);
         }
         
         return hasKey ;
@@ -55,11 +60,104 @@ public class LruCache<TKey, TValue>: ICache<TKey, TValue>
 
     public bool TrySetValue(TKey key, TValue value)
     {
-        throw new NotImplementedException();
+        bool hasKey = LruQueue.TryGetValue(key, out Node <TKey, TValue> node);
+        //Case 1 Existing key => Update value then move to the head
+        if (hasKey)
+        {
+            node.Value = value;
+            MoveToHead(node);
+            return true;
+        }
+        else
+        {
+            //Create a node to be referenced later
+            var new_node = new Node<TKey, TValue>(key, value);
+            
+            //Case 2 New key and under cache capacity => Move to head
+            int currentCapacity = LruQueue.Count;
+            
+            if (currentCapacity < _cacheCapacity)
+            {
+                InsertAtHead(new_node);
+                LruQueue.Add(key, new_node);
+            }
+            else
+            {
+                //Case 3 New key and cache capacity => Evict and move to head
+                //Evict
+                if (_head == _tail)
+                {
+                    LruQueue.Remove(_tail.Key);
+                    _head = null; 
+                    _tail = null;
+                }
+                else
+                {
+                    LruQueue.Remove(_tail.Key);
+                    _tail.Prev.Next = null;
+                    _tail = _tail.Prev;
+                }
+                
+                //To move to head
+                InsertAtHead(new_node);
+                LruQueue.Add(key, new_node);
+            }
+
+            return true;
+
+        }
+        return false;
     }
 
     public bool TryRemoveValue(TKey key)
     {
         throw new NotImplementedException();
+    }
+
+    //HELPER METHODS
+    private void MoveToHead(Node<TKey, TValue> node)
+    {
+        //If the node is the head
+        if (node == _head)
+        {
+            return;
+        }
+        
+        //Unlink the node
+        // If the Node is tail
+        if (node.Next == null)
+        {
+            node.Prev.Next = null;
+            _tail = node.Prev;
+        }
+        else
+        {
+            //if in between two nodes
+            node.Prev.Next = node.Next;
+            node.Next.Prev = node.Prev;
+        }
+        
+        //Link the new head
+        _head.Prev = node;
+        node.Next = _head;
+        node.Prev = null;
+        _head = node;
+    }
+
+    private void InsertAtHead(Node<TKey, TValue> node)
+    {
+        if (_head == null)
+        {
+            _head = node;
+            _tail = node;
+        }
+        else
+        {
+            //Link the new head
+            _head.Prev = node;
+            node.Next = _head;
+            node.Prev = null;
+            _head = node;
+        }
     }
 }
