453a08c4dc7b242aa7c831e0a09fa2075d6c47db
Bathandwa <bmap750@gmail.com>
2026-09-22 10:06:27 +0200

    test(TrySetValue): Implemented TrySetValue_NewKeyUnderCapacity_BecomesHea to assert the TrySetValue adds correctly whilst under capacity.

--- CacheLibrary.Tests/LruCacheTests.cs
+++ CacheLibrary.Tests/LruCacheTests.cs
@@ -5,6 +5,7 @@ namespace CacheLibrary.Tests;
 
 public class LruCacheTests
 {
+    //TryGetValue Tests
     [Fact]
     public void TryGetValue_Does_Not_Exist_Returns_False()
     {
@@ -18,4 +19,22 @@ public class LruCacheTests
         Assert.False(found);
         Assert.Equal(0, value);
     }
+    
+    //TrySetValue Tests
+    [Fact]
+    public void TrySetValue_NewKeyUnderCapacity_BecomesHead()
+    {
+        //Arrange
+        var cache = new LruCache<string, int>(3);
+        
+        //Act
+        var setResult = cache.TrySetValue("banana", 1);
+        cache.TryGetValue("banana", out var value);
+        
+        //Assert
+        Assert.True(setResult);
+        Assert.Equal(1, value);
+    }
+    
+    //TryRemoveValue Tests
 }
