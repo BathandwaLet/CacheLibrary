534c85434d130b6804205e03847ec27b56ba1d3d
Bathandwa <bmap750@gmail.com>
2026-09-24 20:53:50 +0200

    fix(LfuNode): Overriden the next and prev properties of the base class(Node).

--- CacheLibrary.Core/LfuNode.cs
+++ CacheLibrary.Core/LfuNode.cs
@@ -3,6 +3,8 @@ namespace CacheLibrary.Core;
 public class LfuNode <TKey, TValue> : Node<TKey, TValue>
 {
     public int Count { get; set; }
+    public new LfuNode<TKey, TValue>? Prev { get; set; }
+    public new LfuNode<TKey, TValue>? Next { get; set; }
     
     public LfuNode(TKey key, TValue value):base(key, value)
     {
